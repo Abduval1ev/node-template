f22faffcdcb441f484850ea119112e6245bca7c2
Abdumannof <Abdumannof>
2024-05-06 18:28:39 +0300

    A function was created using axios and fetch methods

--- date.js
+++ date.js
@@ -1,7 +1,36 @@
-let date = new Date()
-let string = "It's time now is"
+// let date = new Date()
+// let string = "It's time now is"
 
-let dateMessage = `${string} ${date}`
+// let dateMessage = `${string} ${date}`
 
-module.exports = dateMessage
+// module.exports = dateMessage
 
+// !
+const axios = require("axios");
+const fetch = require("fetch");
+
+const API = "https://jsonplaceholder.typicode.com/posts";
+
+function getPostsFromFetch() {
+  fetch(API)
+    .then((res) => res.json())
+    .then((posts) => {
+      posts.slice(0, 14).map((post) => {
+        return console.log(post.id), console.log(post.userId), console.log(post.title), console.log(post.body);
+      });
+    })
+    .catch((err) => console.log(err));
+}
+getPostsFromFetch();
+
+// ! Axios
+function getPostsFromAxios() {
+  axios(API)
+    .then((posts) => {
+      posts.data.slice(0, 14).map((post) => {
+        return console.log(post.id), console.log(post.userId), console.log(post.title), console.log(post.body);
+      });
+    })
+    .catch((err) => console.log(err));
+}
+getPostsFromAxios();
